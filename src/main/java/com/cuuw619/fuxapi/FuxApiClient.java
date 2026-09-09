7660ddc67bc646ea40918d6f28cfc747f597d56f
cuuw619-maker <cuuw619@gmail.com>
2026-09-09 23:49:49 +0300

    Register client event subscriber for keybind and tick events

--- src/main/java/com/cuuw619/fuxapi/FuxApiClient.java
+++ src/main/java/com/cuuw619/fuxapi/FuxApiClient.java
@@ -8,12 +8,14 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import org.lwjgl.glfw.GLFW;
 
 @Mod(value = FuxApi.MOD_ID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = FuxApi.MOD_ID, value = Dist.CLIENT)
 public final class FuxApiClient {
     public static final String KEY_CATEGORY = "key.categories.fuxapi";
 
