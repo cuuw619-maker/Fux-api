716c886c1103bdbbe87fb8a94dccccd841d72d56
cuuw619-maker <cuuw619@gmail.com>
2026-09-10 12:04:41 +0300

    Add Pulse-style damage numbers module

---
+++ src/main/java/cuuw619/fuxapi/module/FuxDamageNumbersModule.java
@@ -0,0 +1,5 @@
+package com.cuuw619.fuxapi.module;
+
+public final class FuxDamageNumbersModule extends FuxModule {
+    public FuxDamageNumbersModule() { super("Damage Numbers", "Render", false); }
+}
